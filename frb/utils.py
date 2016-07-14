6685754d89dbf4db0f711aa4e18d7c0bcf8edc36
Ilya Pashchenko <in4pashchenko@gmail.com>
2016-07-14 14:34:43 +0300

    some new functions

--- frb/utils.py
+++ frb/utils.py
@@ -6,6 +6,7 @@ from sklearn.mixture import DPGMM, GMM
 from sklearn.cluster import DBSCAN
 from astropy.stats import mad_std, biweight_location
 from scipy.stats import rayleigh
+from scipy import signal
 
 vround = np.vectorize(round)
 vint = np.vectorize(int)
@@ -61,6 +62,7 @@ def find_noisy(dsp, n_max_components, frac=1, alpha=0.1):
     return components_dict, dsp_classified
 
 
+# FIXME: When # amplitudes is small enough, ``eps`` becomes too large...
 def find_clusters_ell_amplitudes(amplitudes, min_samples=10, leaf_size=5,
                                  eps=None):
     """
@@ -86,6 +88,7 @@ def find_clusters_ell_amplitudes(amplitudes, min_samples=10, leaf_size=5,
     data_range = np.max(data) - np.min(data)
     if eps is None:
         eps = data_range / np.sqrt(len(amplitudes))
+        print "eps {}".format(eps)
     db = DBSCAN(eps=eps, min_samples=min_samples,
                 leaf_size=leaf_size).fit(data_)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -98,12 +101,103 @@ def find_clusters_ell_amplitudes(amplitudes, min_samples=10, leaf_size=5,
     outliers = data[labels == -1]
     params = rayleigh.fit(largest_cluster_data)
     distr = rayleigh(loc=params[0], scale=params[1])
-    threshold = distr.ppf(0.99)
+    threshold = distr.ppf(0.999)
 
     # Need data > threshold to ensure that low power outliers haven't been
     # included
     indx = np.logical_and(labels == -1, data > threshold)
+    # return threshold
     return min(data[indx]) - eps
+
+
+def moving_average(data, window_size):
+    """
+    See scipy.sinal window functions also.
+    :param data:
+    :param window_size:
+    :return:
+    """
+    window = np.ones(int(window_size))/float(window_size)
+    return np.convolve(data, window, 'same')
+
+
+def gauss_kern(size, sizey=None):
+    """ Returns a normalized 2D gauss kernel array for convolutions """
+    size = int(size)
+    if not sizey:
+        sizey = size
+    else:
+        sizey = int(sizey)
+    x, y = np.mgrid[-size:size+1, -sizey:sizey+1]
+    g = np.exp(-(x**2/float(size)+y**2/float(sizey)))
+    return g / g.sum()
+
+
+def blur_image(im, n, ny=None) :
+    """ blurs the image by convolving with a gaussian kernel of typical
+        size n. The optional keyword argument ny allows for a different
+        size in the y direction.
+    """
+    g = gauss_kern(n, sizey=ny)
+    improc = signal.convolve(im,g, mode='valid')
+    return improc
+
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
 
 
 def find_robust_gaussian_params(data):
